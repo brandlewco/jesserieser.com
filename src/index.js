ba4cd8e8418018fc5b464f2ee844f6228aa7191d
Brett Lewis <hello@lewcrew.co>
2019-07-01 20:48:42 -0700

    add colleciton page grid

--- src/index.js
+++ src/index.js
@@ -7,141 +7,7 @@ import SwupScrollPlugin from "@swup/scroll-plugin";
 import "lazysizes";
 import "lazysizes/plugins/unveilhooks/ls.unveilhooks";
 import Midday from "midday.js";
-import Shuffle from "shufflejs";
-
-// Sorter
-var Sort = function (element) {
-  this.cats = Array.from(document.querySelectorAll('.js-cats input'));
-  this.filters = Array.from(document.querySelectorAll('.js-filters button'));
-
-  this.shuffle = new Shuffle(element, {
-    easing: 'cubic-bezier(0.165, 0.840, 0.440, 1.000)', // easeOutQuart
-    sizer: '.the-sizer',
-  });
-
-  this.filters = {
-    cats: [],
-    filters: [],
-  };
-
-  this._bindEventListeners();
-};
-
-/**
- * Bind event listeners for when the filters change.
- */
-Sort.prototype._bindEventListeners = function () {
-  this._onShapeChange = this._handleShapeChange.bind(this);
-  this._onColorChange = this._handleColorChange.bind(this);
-
-  this.cats.forEach(function (input) {
-    input.addEventListener('change', this._onShapeChange);
-  }, this);
-
-  this.filters.forEach(function (button) {
-    button.addEventListener('click', this._onColorChange);
-  }, this);
-};
-
-/**
- * Get the values of each checked input.
- * 
- */
-Sort.prototype._getCurrentShapeFilters = function () {
-  return this.cats.filter(function (input) {
-    return input.checked;
-  }).map(function (input) {
-    return input.value;
-  });
-};
-
-/**
- * Get the values of each active button.
- * 
- */
-Sort.prototype._getCurrentColorFilters = function () {
-  return this.filters.filter(function (button) {
-    return button.classList.contains('active');
-  }).map(function (button) {
-    return button.getAttribute('data-value');
-  });
-};
-
-/**
- * A cat input check state changed, update the current filters and filte.r
- */
-Sort.prototype._handleShapeChange = function () {
-  this.filters.cats = this._getCurrentShapeFilters();
-  this.filter();
-};
-
-/**
- * A filter button was clicked. Update filters and display.
- *
- */
-Sort.prototype._handleColorChange = function (evt) {
-  var button = evt.currentTarget;
-
-  // // Treat these buttons like radio buttons where only 1 can be selected.
-  // if (button.classList.contains('active')) {
-  //   button.classList.remove('active');
-  // } else {
-  //   this.filters.forEach(function (btn) {
-  //     btn.classList.remove('active');
-  //   });
-
-  //   button.classList.add('active');
-  // }
-
-  this.filters.filters = this._getCurrentColorFilters();
-  this.filter();
-};
-
-/**
- * Filter shuffle based on the current state of filters.
- */
-Sort.prototype.filter = function () {
-  if (this.hasActiveFilters()) {
-    this.shuffle.filter(this.itemPassesFilters.bind(this));
-  } else {
-    this.shuffle.filter(Shuffle.ALL_ITEMS);
-  }
-};
-
-/**
- * If any of the arrays in the filters property have a length of more than zero,
- * that means there is an active filter.
- * 
- */
-Sort.prototype.hasActiveFilters = function () {
-  return Object.keys(this.filters).some(function (key) {
-    return this.filters[key].length > 0;
-  }, this);
-};
-
-/**
- * Determine whether an element passes the current filters.
- *
- */
-Sort.prototype.itemPassesFilters = function (element) {
-  var cats = this.filters.cats;
-  var filters = this.filters.filters;
-  var cat = element.getAttribute('data-cat');
-  var filter = element.getAttribute('data-filter');
-
-  // If there are active cat filters and this cat is not in that array.
-  if (cats.length > 0 && !cats.includes(cat)) {
-    return false;
-  }
-
-  // If there are active filter filters and this filter is not in that array.
-  if (filters.length > 0 && !filters.includes(filter)) {
-    return false;
-  }
-
-  return true;
-};
-
+import Isotope from "isotope-layout";
 
 // Page Loader (SWUP)
 const options = {
@@ -183,7 +49,82 @@ function init() {
     sectionSelector: "hue"
   });
 
-  const sort = new Sort(document.getElementById("grid"));
+  // Sorter
+  // filter functions
+  var filterFns = {
+    greaterThan50: function(itemElem) {
+      var number = itemElem.querySelector(".number").textContent;
+      return parseInt(number, 10) > 50;
+    },
+    even: function(itemElem) {
+      var number = itemElem.querySelector(".number").textContent;
+      return parseInt(number, 10) % 2 === 0;
+    }
+  };
+
+  // store filter for each group
+  var filters = {};
+
+  // init Isotope
+  var iso = new Isotope(".collection-sort", {
+    itemSelector: ".collection-item",
+    filter: function(itemElem) {
+
+      var isMatched = true;
+
+      for (var prop in filters) {
+        var filter = filters[ prop ];
+        // use function if it matches
+        filter = filterFns[ filter ] || filter;
+        // test each filter
+        var filterType = typeof filter;
+        if (filter && filterType == "function") {
+          isMatched = filter(itemElem);
+        } else if (filter) {
+          isMatched = matchesSelector(itemElem, filter);
+        }
+        // break if not matched
+        if (!isMatched) {
+          break;
+        }
+      }
+      return isMatched;
+    }
+
+  });
+
+  document.querySelector("#filters").addEventListener("click", function(event) {
+  // only work with buttons
+    if (!matchesSelector(event.target, "button")) {
+      return;
+    }
+    // get group key
+    var buttonGroup = event.target.parentNode;
+    var filterGroup = buttonGroup.getAttribute("data-filter-group");
+    // set filter for group
+    filters[ filterGroup ] = event.target.getAttribute("data-filter");
+    // arrange, and use filter fn
+    iso.arrange();
+  });
+
+  // change is-checked class on buttons
+  var buttonGroups = document.querySelectorAll(".button-group");
+  for (var i = 0; i < buttonGroups.length; i++) {
+    var buttonGroup = buttonGroups[i];
+    radioButtonGroup(buttonGroup);
+  }
+
+  function radioButtonGroup(buttonGroup) {
+    buttonGroup.addEventListener("click", function(event) {
+    // only work with buttons
+      if (!matchesSelector(event.target, "button")) {
+        return;
+      }
+      buttonGroup.querySelector(".is-checked").classList.remove("is-checked");
+      event.target.classList.add("is-checked");
+    });
+  }
+
 }
 init();
 
