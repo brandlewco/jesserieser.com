1e9b3bc5235c4e697d3800a53d1475fee0480f59
Brett Lewis <hello@lewcrew.co>
2020-01-09 14:35:37 -0700

    test purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,14 @@
+const purgecss = require("@fullhuman/postcss-purgecss");
+
 module.exports = {
   plugins: [
     require("postcss-import")(),
     require("postcss-preset-env")(),
     require("autoprefixer"),
     require("postcss-nested")(),
-    require("tailwindcss")
+    require("tailwindcss"),
+    purgecss({
+      content: ["./**/*.html"]
+    })
   ]
 };
