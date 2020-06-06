7b80b07154ae608e7c50de31ac70efd710b3b465
Brett Lewis <hello@lewcrew.co>
2020-06-06 13:07:00 -0700

    test focus unfocus

--- src/index.js
+++ src/index.js
@@ -660,6 +660,29 @@ function init() {
       }
     }
   })();
+
+  // accesibility highlighter
+  (function(document, window) {
+    var styleText = "::-moz-focus-inner{border:0 !important;}:focus{outline: none !important;";
+    var unfocus_style = document.createElement("STYLE");
+
+    window.unfocus = function() {
+      document.getElementsByTagName("HEAD")[0].appendChild(unfocus_style);
+
+      document.addEventListener("mousedown", function() {
+        unfocus_style.innerHTML = styleText + "}";
+      });
+      document.addEventListener("keydown", function() {
+        unfocus_style.innerHTML = "";
+      });
+    };
+
+    unfocus.style = function(style) {
+      styleText += style;
+    };
+
+    unfocus();
+  })(document, window);
 }
 
 // intit code on each page load
