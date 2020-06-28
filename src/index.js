8a747352fac2f7fa3c740899ec6116727870cc53
Brett Lewis <hello@lewcrew.co>
2020-06-28 14:41:57 -0700

    test debounce method on nobile browser

--- src/index.js
+++ src/index.js
@@ -60,12 +60,17 @@ function init() {
 
   swup.preloadPages();
 
-  function setDocHeight() {
-    document.documentElement.style.setProperty("--vh", `${window.innerHeight / 100}px`);
-  }
-  setDocHeight();
-  addEventListener("resize", setDocHeight);
-  addEventListener("orientationchange", setDocHeight);
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  const vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+  // We listen to the resize event
+  window.addEventListener("resize", () => {
+    // We execute the same script as before
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  });
 
   const setUp = () => {
     console.log("// built by brett lewis");
@@ -475,7 +480,7 @@ function init() {
       popupModal.classList.remove("is--visible");
       scrollUnlock();
     });
-    
+
     if (popupModal.querySelector(".exit-modal")) {
       console.log("yeah, it's here");
       popupModal.querySelector(".exit-modal").addEventListener("click", () => {
