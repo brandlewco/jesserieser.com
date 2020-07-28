97330455c9b2fd91337bd05e3d75d5fa31078347
Brett Lewis <hello@lewcrew.co>
2020-07-27 18:33:51 -0700

    acccesibility highlight

--- src/index.js
+++ src/index.js
@@ -686,27 +686,27 @@ function init() {
   })();
 
   // accesibility highlighter
-  // (function(document, window) {
-  //   var styleText = "::-moz-focus-inner{border:0 !important;}:focus{outline: none !important;";
-  //   var unfocus_style = document.createElement("STYLE");
+  (function(document, window) {
+    var styleText = "::-moz-focus-inner{border:0 !important;}:focus{outline: none !important;";
+    var unfocus_style = document.createElement("STYLE");
 
-  //   window.unfocus = function() {
-  //     document.getElementsByTagName("HEAD")[0].appendChild(unfocus_style);
+    window.unfocus = function() {
+      document.getElementsByTagName("HEAD")[0].appendChild(unfocus_style);
 
-  //     document.addEventListener("mousedown", function() {
-  //       unfocus_style.innerHTML = styleText + "}";
-  //     });
-  //     document.addEventListener("keydown", function() {
-  //       unfocus_style.innerHTML = "";
-  //     });
-  //   };
+      document.addEventListener("mousedown", function() {
+        unfocus_style.innerHTML = styleText + "}";
+      });
+      document.addEventListener("keydown", function() {
+        unfocus_style.innerHTML = "";
+      });
+    };
 
-  //   unfocus.style = function(style) {
-  //     styleText += style;
-  //   };
+    unfocus.style = function(style) {
+      styleText += style;
+    };
 
-  //   unfocus();
-  // })(document, window);
+    unfocus();
+  })(document, window);
 }
 
 // intit code on each page load
